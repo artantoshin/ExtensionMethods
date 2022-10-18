700211950b76512c08ff89c05498b756908acf52
art.antoshin <art.antoshin1992@gmail.com>
2022-10-18 22:11:01 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿string s = "Привет мир";
+char c = 'и';
+int i = s.CharCount(c);
+Console.WriteLine(i);
+
+public static class StringExtension
+{
+    public static int CharCount(this string str, char c)
+    {
+        int counter = 0;
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == c)
+                counter++;
+        }
+        return counter;
+    }
+}
